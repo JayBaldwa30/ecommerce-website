e56a328ee9b1ed9e3abf276515e46318c238ed65
jaybaldwa30 <jaybaldwa123@gmail.com>
2024-11-25 14:53:50 +0530

    updated store extension

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "../features/apiSlice.js";
-import { cartSlice } from "../features/cartSlice.js";
+import apiSlice from "../features/apiSlice.js";
+import cartSlice from "../features/cartSlice.js";
 
 const store = configureStore({
   reducer: {
