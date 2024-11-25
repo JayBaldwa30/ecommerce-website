418f486550fc2a28b779c9bd37e002abef0cb083
jaybaldwa30 <jaybaldwa123@gmail.com>
2024-11-25 09:50:22 +0530

    added the root layout page

---
+++ src/components/layout/RootLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavigationMenuDemo from "../components/NavigationMenuDemo";
+
+const RootLayout = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <NavigationMenuDemo />
+
+      <main className="p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default RootLayout;
