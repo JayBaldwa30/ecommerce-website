9d1b43de1a0e22f5bc5675ea6c347fb484660848
jaybaldwa30 <jaybaldwa123@gmail.com>
2024-11-25 13:42:48 +0530

    updated the reviewed code

--- src/redux/store/Store.jsx
+++ src/redux/store/Store.jsx
@@ -4,13 +4,13 @@ import { cartSlice } from "../features/cartSlice";
 
 const store = configureStore({
   reducer: {
-    [cartSlice.reducerPath]: cartSlice.reducer, 
-    [apiSlice.reducerPath]: apiSlice.reducer, 
+    [cartSlice.reducerPath]: cartSlice.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(cartSlice.middleware)
-      .concat(apiSlice.middleware), 
+      .concat(apiSlice.middleware),
 });
 
 export default store;
